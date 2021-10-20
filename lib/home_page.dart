2d8643b05d5434e7e331c1ba707f547bee431935
felipesantosds <felipedssantos.dev@gmail.com>
2021-10-20 16:28:57 -0300

    Add text widget

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Hello, World!",
+        ),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: _text(),
+      ),
+    );
+  }
+
+  _text() {
+    return const Text(
+      "Hello, World!",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.blue,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        decoration: TextDecoration.underline,
+        decorationColor: Colors.red,
+        decorationStyle: TextDecorationStyle.wavy,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_01/home_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
